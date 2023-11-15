306cb492008242dd50788cf9cd1f723a24d2b830
MH Sunny <sunny@oraiko.com>
2023-11-15 16:36:37 +0600

    wecome section created

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HomeSlider from "@/components/homeSlider/HomeSlider";
+import WelcomeSection from "@/components/welcomeSection/WelcomeSection";
 
 export default function Home() {
   return (
     <>
       <HomeSlider />
+      <WelcomeSection />
     </>
   );
 }

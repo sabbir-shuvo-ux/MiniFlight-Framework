3b5c8d6cec1916e90f76245bc466a608daa2653f
MH Sunny <sunny@oraiko.com>
2023-11-23 15:50:13 +0600

    using map only in client side

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
+import dynamic from "next/dynamic";
+
+// components
 import ApplicationsSection from "@/components/applicationsSection/ApplicationsSection";
 import BrandsSection from "@/components/brandsSection/BrandsSection";
-import GoogleMap from "@/components/googleMap/GoogleMap";
 import HomeSlider from "@/components/homeSlider/HomeSlider";
 import RecentWorkSection from "@/components/recentWork/RecentWorkSection";
 import ReviewHome from "@/components/reviewHome/ReviewHome";
 import ServicesSection from "@/components/servicesSection/ServicesSection";
 import WelcomeSection from "@/components/welcomeSection/WelcomeSection";
+
+const DynamicMap = dynamic(() => import("@/components/googleMap/GoogleMap"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -17,7 +23,7 @@ export default function Home() {
       <BrandsSection />
       <RecentWorkSection />
       <ReviewHome />
-      <GoogleMap />
+      <DynamicMap />
     </>
   );
 }
